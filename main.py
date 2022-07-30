19fa754b72b39aac246bf385e2ebeff7cf35b1ad
Gergely Imreh <imrehg@gmail.com>
2022-07-31 00:08:17 +0800

    formatting and column type fix

--- main.py
+++ main.py
@@ -7,24 +7,24 @@ from pathlib import Path
 import pandas as pd
 import pikepdf
 import tabula  # type: ignore
-from fastapi import FastAPI, HTTPException, Request
+from fastapi import FastAPI, Request
 from google.appengine.api import mail  # type: ignore
 from pydantic import BaseSettings, validator
 
 # Hack: install Java which is required for the PDF processing
-if shutil.which('java') is None:
-    print('Installing Java....')
+if shutil.which("java") is None:
+    print("Installing Java....")
     import jdk  # type: ignore
 
     try:
-        java_path = Path(jdk.install('11', jre=True)) / 'bin'
+        java_path = Path(jdk.install("11", jre=True)) / "bin"
     except StopIteration:
-        jdk.uninstall('11', jre=True)
-        java_path = Path(jdk.install('11', jre=True)) / 'bin'
+        jdk.uninstall("11", jre=True)
+        java_path = Path(jdk.install("11", jre=True)) / "bin"
 
-    os.environ['PATH'] = str(java_path) + ':' + os.environ['PATH']
-    print(f'Java Path: {java_path}')
-    print('Installing Java: Done')
+    os.environ["PATH"] = str(java_path) + ":" + os.environ["PATH"]
+    print(f"Java Path: {java_path}")
+    print("Installing Java: Done")
 # Hack: End
 
 
@@ -41,59 +41,57 @@ def access_secret_version(project_id, secret_id, version_id):
     client = secretmanager.SecretManagerServiceClient()
 
     # Build the resource name of the secret version.
-    name = f'projects/{project_id}/secrets/{secret_id}/versions/{version_id}'
+    name = f"projects/{project_id}/secrets/{secret_id}/versions/{version_id}"
 
     # Access the secret version.
-    response = client.access_secret_version(request={'name': name})
-    payload = response.payload.data.decode('UTF-8')
+    response = client.access_secret_version(request={"name": name})
+    payload = response.payload.data.decode("UTF-8")
     return payload
 
 
 class Settings(BaseSettings):
     google_cloud_project: str
-    huanan_pass: str = ''
-    bigquery_location: str = 'asia-east1'
-    bigquery_dataset: str = 'finance'
-    huanan_table_name: str = 'huanan'
+    huanan_pass: str = ""
+    bigquery_location: str = "asia-east1"
+    bigquery_dataset: str = "finance"
+    huanan_table_name: str = "huanan"
 
-    @validator('huanan_pass')
+    @validator("huanan_pass")
     def load_from_secrets_if_missing(cls, v, values):
-        if v == '':
+        if v == "":
             v = access_secret_version(
-                values['google_cloud_project'], 'huanan_pass', 'latest'
+                values["google_cloud_project"], "huanan_pass", "latest"
             )
         return v
 
     class Config:
-        env_file = '.env'
+        env_file = ".env"
 
 
 settings = Settings()
 app = FastAPI()
 
 
-@app.get('/')
+@app.get("/")
 async def root():
-    return {'message': 'Hello World'}
+    return {"message": "Hello World"}
 
 
-@app.post('/_ah/mail/{address}')
+@app.post("/_ah/mail/{address}")
 async def receive_mail(address: str, request: Request):
     body = await request.body()
     mail_message = mail.InboundEmailMessage(body)
 
     # Do something with the message
-    print('Received greeting at %s from %s.' % (mail_message.to, mail_message.sender))
+    print("Received greeting at %s from %s." % (mail_message.to, mail_message.sender))
 
-    organisation = mail_message.to.split('@')[0]
+    organisation = mail_message.to.split("@")[0]
     match organisation:
-        case 'huanan':
+        case "huanan":
             handle_huanan_data(mail_message)
         case _:
-            raise HTTPException(
-                status_code=404, detail=f"Can't handle request type: {organisation}"
-            )
-    return 'Success'
+            print(f"Can't handle request type: {organisation}")
+    return "Success"
 
 
 def handle_huanan_data(mail_message) -> None:
@@ -102,21 +100,22 @@ def handle_huanan_data(mail_message) -> None:
     except AttributeError:
         for _, encoded_body in mail_message.bodies():
             print(encoded_body.decode())
-        raise HTTPException(status_code=422, detail='No attachments in this email.')
+        print("No attachments in this email.")
+        return
 
     encrypted_pdf = BytesIO(payload.decode())
     pdf = pikepdf.open(encrypted_pdf, password=settings.huanan_pass)
     bio = BytesIO()
     pdf.save(bio)
-    dfs = tabula.read_pdf(bio, pages='all')
+    dfs = tabula.read_pdf(bio, pages="all")
     transactions = dfs[0]
     cleaned_transactions = huanan_df_cleanup(transactions)
 
     print(cleaned_transactions)
 
     cleaned_transactions.to_gbq(
-        destination_table=f'{settings.bigquery_dataset}.{settings.huanan_table_name}',
-        if_exists='append',
+        destination_table=f"{settings.bigquery_dataset}.{settings.huanan_table_name}",
+        if_exists="append",
         location=settings.bigquery_location,
     )
 
@@ -124,9 +123,9 @@ def handle_huanan_data(mail_message) -> None:
 def huanan_df_cleanup(df_in):
     df_transactions = df_in.copy()
     column_names_map = {
-        0: 'CardLastDigits',
-        1: 'TransactionDate',
-        2: 'TransactionAmountNTD',
+        0: "CardLastDigits",
+        1: "TransactionDate",
+        2: "TransactionAmountNTD",
     }
     column_names = [
         column_names_map[col_index] for col_index in sorted(column_names_map.keys())
@@ -139,13 +138,14 @@ def huanan_df_cleanup(df_in):
     df_transactions = df_transactions.rename(columns=rename_map)
     df_transactions = df_transactions[column_names]
 
-    df_transactions[['TransactionAmountNTD', 'TransactionType']] = df_transactions[
-        'TransactionAmountNTD'
+    df_transactions[["TransactionAmountNTD", "TransactionType"]] = df_transactions[
+        "TransactionAmountNTD"
     ].str.split(expand=True)
-    df_transactions['TransactionDate'] = pd.to_datetime(
-        df_transactions['TransactionDate'], utc=False
-    ).dt.tz_localize('Asia/Taipei')
-    df_transactions['TransactionAmountNTD'] = df_transactions[
-        'TransactionAmountNTD'
-    ].apply(lambda value: int(value.replace(',', '')))
+    df_transactions["TransactionDate"] = pd.to_datetime(
+        df_transactions["TransactionDate"], utc=False
+    ).dt.tz_localize("Asia/Taipei")
+    df_transactions["TransactionAmountNTD"] = df_transactions[
+        "TransactionAmountNTD"
+    ].apply(lambda value: int(value.replace(",", "")))
+    df_transactions["TransactionType"] = df_transactions["TransactionType"].astype(int)
     return df_transactions
--- test_main.py
+++ test_main.py
@@ -7,51 +7,51 @@ client = TestClient(app)
 
 
 def test_read_main():
-    response = client.get('/')
+    response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {'message': 'Hello World'}
+    assert response.json() == {"message": "Hello World"}
 
 
-@pytest.mark.skip('Not written')
+@pytest.mark.skip("Not written")
 def test_post_email_with_not_supported_address():
     pass
 
 
-@pytest.mark.skip('Not written')
+@pytest.mark.skip("Not written")
 def test_post_email_to_supported_endpoint_without_attachment():
     pass
 
 
-@pytest.mark.skip('Not written')
+@pytest.mark.skip("Not written")
 def test_post_email_to_supported_endpoint_with_not_pdf():
     pass
 
 
-@pytest.mark.skip('Not written')
+@pytest.mark.skip("Not written")
 def test_post_email_to_supported_endpoint_with_not_encrypted_pdf():
     pass
 
 
-@pytest.mark.skip('Not written')
+@pytest.mark.skip("Not written")
 def test_post_email_to_supported_endpoint_with_wrong_password():
     pass
 
 
-@pytest.mark.skip('Not written')
+@pytest.mark.skip("Not written")
 def test_post_email_to_supported_endpoint_with_correct_pdf_no_table():
     pass
 
 
-@pytest.mark.skip('Not written')
+@pytest.mark.skip("Not written")
 def test_post_email_to_supported_endpoint_with_correct_pdf_right_table():
     pass
 
 
-@pytest.mark.skip('Not written')
+@pytest.mark.skip("Not written")
 def test_pdf_to_table_conversion():
     pass
 
 
-@pytest.mark.skip('Not written')
+@pytest.mark.skip("Not written")
 def test_data_table_to_bigquery():
     pass
